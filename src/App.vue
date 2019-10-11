0d77d1c2a98452ab2f4afcf4b7c8c0a7be557d2d
yxp1995 <972307655@qq.com>
2019-10-11 11:10:40 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>App----</h1>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  
+}
+</script>
+
+<style lang="less">
+
+</style>
